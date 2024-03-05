40f978cb820dac58653ca1f3e3b1e98bcfa50ca3
mordecai <142639542+m0rd3caii@users.noreply.github.com>
2024-03-05 01:28:26 +0100

    Update win-passwd-stealer.ps1

--- win-passwd-stealer.ps1
+++ win-passwd-stealer.ps1
@@ -1,16 +1,16 @@
-# Obtener los perfiles de red inalámbrica
+# Get wireless network profiles
 $profiles = netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
 
-# Obtener las contraseñas de los perfiles
+# Get profile passwords
 foreach ($profile in $profiles) {
     try {
         $password_lines = netsh wlan show profile $profile key=clear | Select-String "Key Content" | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
         if ($password_lines) {
             Write-Host "$profile | $($password_lines[0])"
         } else {
-            Write-Host "$profile | No se encontró contraseña"
+            Write-Host "$profile | Password not found"
         }
     } catch {
-        Write-Host "Error al obtener la contraseña para el perfil: $profile"
+        Write-Host "Error getting password for profile: $profile"
     }
 }
